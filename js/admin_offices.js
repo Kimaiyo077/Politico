f89d05c63cfccdfd6ee2e4ea3f758a7213b6aff1
Isaac Kimaiyo <biwot777@gmail.com>
2019-03-05 11:20:02 +0300

    [Starts #164394116] allow user to view all governemnt offices

---
+++ js/admin_offices.js
@@ -0,0 +1,58 @@
+window.addEventListener('load', getAllOffices)
+
+function getAllOffices(){
+    let token = localStorage.getItem('token')
+
+    fetch('http://127.0.0.1:5000/api/v2/offices', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json, */*',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token       
+        },  
+    })
+
+    .then((res) => {
+        return res.json()
+    })
+
+    .then((data) => {
+        statusCode = data.status
+        let offices =  data.data;
+
+        
+        if (statusCode == 200){
+            offices.forEach(office => {
+                id = office.officeId
+                name = office.officeName
+                type = office.officeType
+
+                let output = `
+                    <li>
+                        <h2><span>Office: </span>${name}</h2>
+                        <h2><span>Type: </span>${type}</h2>
+                        <i class="fas fa-vote-yea fa-10x"></i>
+                        <button type="button" name="${id}" id="${id}" onclick="setId(this.id)"><i class="far fa-edit"></i></button>
+                        <button type="button" name="${id}" id="${id}" onclick="deleteOffice(this.id)"><i class="fas fa-trash"></i></button>
+                    </li>
+                `;
+                document.getElementById('content').innerHTML += output;
+            });
+            
+        }
+
+        if (statusCode == 401){
+            console.log(data.error)
+            confirm(data.error)
+        }
+
+        if (statusCode == 404){
+            document.getElementById("message").style.backgroundColor = 'red';
+            document.getElementById("message").style.color = 'white';
+            document.getElementById("message").innerHTML = data.error;
+        }
+
+    })
+
+    .catch((error) => console.log(error))
+}
