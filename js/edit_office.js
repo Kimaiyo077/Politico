45a461f9a35fcab66d0e87dd0ab44cea7aed3b96
Isaac Kimaiyo <biwot777@gmail.com>
2019-03-05 10:17:55 +0300

    [Starts #164393294] allow admin to edit office name

---
+++ js/edit_office.js
@@ -0,0 +1,40 @@
+document.getElementById('editOffice').addEventListener('submit', editOffice)
+
+function editOffice(event){
+    event.preventDefault();
+
+    let name = document.getElementById('officeName').value;
+    let token = localStorage.getItem('token');
+    let id = localStorage.getItem('officeId');
+
+    let officeData = {
+        'name' : name
+    }
+
+    fetch('http://127.0.0.1:5000/api/v2/offices/1',{
+        method : 'PATCH',
+        headers : {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type':'application/json',
+            'Authorization': 'Bearer ' + token 
+        },
+        body : JSON.stringify(officeData)
+    })
+
+        .then((res) => {
+            return res.json()
+        })
+
+        .then((data) => {
+            if (data.status == 200){
+                window.setTimeout(() => window.location.replace('admin_parties.html'), 1200);
+            }else{
+                document.getElementById("message").style.backgroundColor = 'red';
+                document.getElementById("message").style.color = 'white';
+                document.getElementById("message").innerHTML = data.error;
+            }
+        })
+
+        .catch((error) => console.log(error))
+
+}
